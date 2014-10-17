4abadd00e9b7d68544453a496d52ba5943245430
jaclynpasquale <jaclynpasquale@gmail.com>
2014-10-17 16:02:58 -0500

    progress  with '#' will be ignored, and an empty message aborts the commit.

--- volunterism.js
+++ volunterism.js
@@ -1,16 +1,49 @@
 document.addEventListener('DOMContentLoaded', function(){
-var $select = document.querySelector('select');
-var $button = document.querySelector('button');
+	var $select = document.querySelector('select');
+	var $button = document.querySelector('button');
 
-$button.addEventListener('click', function(){
-if ($select.value === 'Random Student'){
-alert($select.value);
+ 	$button.addEventListener('click', function(){
+   		var $target = document.querySelector('.target');
+   		var docFragment = createPTag();
+    		$target.appendChild(docFragment);
+ 	 });
 
+
+
+	$button.addEventListener('click', function(){
+		if ($select.value === 'Random Student'){
+			var students = ['joe', 'sue', 'jim', 'tom', 'dick', 'harry', 'jane', 'june','may','martha', 'jack']
+			var showRandomStudent = students[Math.floor(Math.random() * 10) + 1];
+
+			alert(showRandomStudent)
+		} else if ($select.value === 'Neighbor Pairing'){
+			var students = ['joe', 'sue', 'jim', 'tom', 'dick', 'harry', 'jane', 'june','may','martha', 'jack']
+			
+			var pair = []
+			for (var i = 0; i < students.length; i++) {
+				var number = Math.floor(Math.random() * students.length) -1
+				var showNeighborPairs = students[number]; 
+				if (pair.length < 2){
+					pair.push(showNeighborPairs);
+					students.splice(number, 1);
+				} else {pair = []}
+   
+			}
+			alert(pair)
+			
+		}
+		});
+		
+});
+function createPTag(){
+  var docFragment = document.createDocumentFragment();
+
+  var $div = document.createElement('div');
+  $div.setAttribute('class', 'myClass');
+  var $p = document.createElement('p');
+  $div.appendChild($p);
+  $p.textContent = 'Hi';
+  docFragment.appendChild($div);
+
+  return docFragment;
 }
-
-
-
-});
-
-});
-
