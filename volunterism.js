12bdef5c9ac172db31a6cacd0721f3ecc24698e5
jaclynpasquale <jaclynpasquale@gmail.com>
2014-10-17 12:16:00 -0500

    alert on random student

--- volunterism.js
+++ volunterism.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+var $select = document.querySelector('select');
+var $button = document.querySelector('button');
+
+$button.addEventListener('click', function(){
+if ($select.value === 'Random Student'){
+alert($select.value);
+
+}
+
+
+
+});
 
 });
 
